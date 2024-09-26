9665559975cd868b75b6b44270b1ffd056ce4b0c
Felicia Chan <feliciac@davyntt.com>
2024-09-26 18:52:23 -0400

    set up user polymorphic behaviour

--- App/models/user.py
+++ App/models/user.py
@@ -2,9 +2,15 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    type = db.Column(db.String(50))
+    __mapper_args__ = {
+        'polymorphic_identity': 'user', 
+        'polymorphic_on': type
+    }
 
     def __init__(self, username, password):
         self.username = username
