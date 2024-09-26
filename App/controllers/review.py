c64e83455e1ad739718634ae30851e6adc19a53f
Felicia Chan <feliciac@davyntt.com>
2024-09-26 00:42:10 -0400

    applied new models

--- App/controllers/review.py
+++ App/controllers/review.py
@@ -1,11 +1,18 @@
-from App.models import Review
+from App.models import Review, Staff, Student
 from App.database import db
 
-def create_review(student_id, staff_id, is_positive, rating, comment):
+def create_review(student_id, staff_id, rating, comment):
+    staff: Staff | None = Staff.query.filter_by(staff_id=staff_id).first()
+    if not staff:
+        print(f'Staff with ID [ {staff_id} ] not found !')
+        
+    student: Student | None = Student.query.filter_by(student_id=student_id).first()
+    if not staff:
+        print(f'Student with ID [ {student_id} ] not found !')
+    
     new_review = Review(
-        student_id=student_id, 
-        staff_id=staff_id, 
-        is_positive=is_positive, 
+        student_id=student.student_id, 
+        staff_id=staff.staff_id, 
         rating=rating, 
         comment=comment
     )
--- App/controllers/student.py
+++ App/controllers/student.py
@@ -2,6 +2,10 @@ from App.models import Student
 from App.database import db
 
 def create_student(student_id, firstname, lastname):
+    student = Student.query.filter_by(student_id=student_id).first()
+    if student:
+        print(f'The student ID [ {student_id} ] is already exists !')
+        return
     new_student = Student(
         student_id=student_id, 
         firstname=firstname, 
@@ -9,7 +13,27 @@ def create_student(student_id, firstname, lastname):
     )
     db.session.add(new_student)
     db.session.commit()
+    print(f'{student_id} | {firstname} {lastname} added !')
+    return
     
     
 def get_all_students():
     return Student.query.all()
+
+
+def search_student_by_name(name):
+    all_students = get_all_students()
+    students = []
+    for student in all_students:
+        if student.firstname == name or student.lastname == name:
+            students.append(student)
+    return students
+
+
+def search_student_by_id(student_id):
+    return Student.query.filter_by(student_id=student_id).first()
+
+
+def get_student_reviews(student_id):
+    student = Student.query.filter_by(student_id=student_id).first()
+    return student.get_reviews()
