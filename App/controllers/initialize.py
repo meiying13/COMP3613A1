bc53e076588b2dce03df0129b615a20235709d49
Felicia Chan <feliciac@davyntt.com>
2024-09-26 00:42:42 -0400

    modified seeder to fit updated models

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -16,12 +16,13 @@ def initialize():
     # Create Staff
     create_staff("00000611", "Alice", "Brown")
     create_staff("00000612", "Bob", "Miller")
+    create_staff("00000613", "Bob", "Ross")
 
     # Create Reviews
-    create_review("81600000", "00000611", is_positive=True, rating=5, comment="Excellent performance!")
-    create_review("81600000", "00000612", is_positive=False, rating=2, comment="Needs improvement in communication.")
-    create_review("81600001", "00000611", is_positive=True, rating=4, comment="Good student, works hard.")
-    create_review("81600001", "00000612", is_positive=False, rating=3, comment="Satisfactory but can do better.")
+    create_review("81600000", "00000611", rating=5, comment="Excellent performance!")
+    create_review("81600000", "00000612", rating=2, comment="Needs improvement in communication.")
+    create_review("81600001", "00000611", rating=4, comment="Good student, works hard.")
+    create_review("81600001", "00000612", rating=3, comment="Satisfactory but can do better.")
     
     
     
