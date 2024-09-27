fcd2d2c706e7ebf76920113f63895d9533c45b19
Felicia Chan <feliciac@davyntt.com>
2024-09-26 20:37:24 -0400

    refactor

--- App/models/staff.py
+++ App/models/staff.py
@@ -8,7 +8,6 @@ class Staff(User):
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
     written_reviews = db.relationship('Review', backref='author', lazy=True)
-
     __mapper_args__ = {
         'polymorphic_identity': 'staff'
     }
@@ -20,14 +19,15 @@ class Staff(User):
         self.lastname = lastname
         
     def __repr__(self):
-        return f'<Staff: {self.staff_id} | {self.get_fullname()}>'
+        return f'<Staff: {self.user.id} | {self.staff_id} | {self.get_fullname()}>'
     
     def get_fullname(self):
         return f'{self.firstname} {self.lastname}'
 
     def get_json(self):
-        student_reviews = self.get_written_reviews()
+        student_reviews = [review.get_json() for review in self.written_reviews]
         return {
+            'id': self.user.id,
             'staff_id': self.staff_id,
             'firstname': self.firstname,
             'lastname': self.lastname,
