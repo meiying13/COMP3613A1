5b4a4bbe286f76364b0784d8174eb8018bd742ca
Felicia Chan <feliciac@davyntt.com>
2024-09-27 19:51:22 -0400

    modified to read csv files

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,26 +1,32 @@
-import json
+import csv
 from .user import create_user
 from .student import create_student
 from .staff import create_staff
+from .admin import create_admin
 from .review import create_review
 from App.database import db
 
 def initialize():
     db.drop_all()
     db.create_all()
+    
+    # Load seed data from CSV files
+    with open('students.csv') as students_file:
+        reader = csv.DictReader(students_file)
+        for row in reader:
+            create_student(row["student_id"], row["firstname"], row["lastname"])
 
-    # Load seed data from JSON
-    with open('data.json', 'r') as file:
-        data = json.load(file)
+    with open('staff.csv') as staff_file:
+        reader = csv.DictReader(staff_file)
+        for row in reader:
+            create_staff(row["staff_id"], row["password"], row["firstname"], row["lastname"])
+            
+    with open('admins.csv') as admins_file:
+        reader = csv.DictReader(admins_file)
+        for row in reader:
+            create_admin(row["username"], row["password"])
 
-    # Create Students
-    for student in data["students"]:
-        create_student(student["student_id"], student["firstname"], student["lastname"])
-
-    # Create Staff
-    for staff in data["staff"]:
-        create_staff(staff["staff_id"], staff["password"], staff["firstname"], staff["lastname"])
-
-    # Create Reviews
-    for review in data["reviews"]:
-        create_review(review["student_id"], review["staff_id"], review["rating"], review["comment"])
+    with open('reviews.csv') as reviews_file:
+        reader = csv.DictReader(reviews_file)
+        for row in reader:
+            create_review(row["student_id"], row["staff_id"], int(row["rating"]), row["comment"])
