c68b6436701c12935d9b666216dc0bf33663e3d5
Felicia Chan <feliciac@davyntt.com>
2024-09-27 22:38:12 -0400

    added types

--- App/models/user.py
+++ App/models/user.py
@@ -2,31 +2,31 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(120), nullable=False)
-    type = db.Column(db.String(50))
+    __tablename__: str = "user"
+    id: str = db.Column(db.Integer, primary_key=True)
+    username: str =  db.Column(db.String(20), nullable=False, unique=True)
+    password: str = db.Column(db.String(120), nullable=False)
+    type: str = db.Column(db.String(50))
     __mapper_args__ = {
         'polymorphic_identity': 'user', 
         'polymorphic_on': type
     }
 
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str) -> None:
         self.username = username
         self.set_password(password)
 
-    def get_json(self):
+    def get_json(self) -> dict[str, str]:
         return{
             'id': self.id,
             'username': self.username
         }
 
-    def set_password(self, password):
+    def set_password(self, password: str) -> None:
         """Create hashed password."""
         self.password = generate_password_hash(password)
     
-    def check_password(self, password):
+    def check_password(self, password: str) -> bool:
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
