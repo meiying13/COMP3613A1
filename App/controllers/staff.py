cb00c827612594ad72848743b7bb43b49810d55e
Felicia Chan <feliciac@davyntt.com>
2024-09-27 20:10:02 -0400

    updated staff model

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -2,6 +2,10 @@ from App.models import Staff
 from App.database import db
 
 def create_staff(staff_id, password, firstname, lastname):
+    staff = Staff.query.filter_by(staff_id=staff_id).first()
+    if staff:
+        print(f'The staff ID [ {staff_id} ] already exists !')
+        return
     new_staff = Staff(
         staff_id=staff_id, 
         password=password,
@@ -10,7 +14,23 @@ def create_staff(staff_id, password, firstname, lastname):
     )
     db.session.add(new_staff)
     db.session.commit()
-    
+    print(f'{staff_id} | {firstname} {lastname} added !')
+        
     
 def get_all_staff():
     return Staff.query.all()
+
+
+def get_staff_by_id(staff_id):
+    staff = Staff.query.filter_by(staff_id=staff_id).first()
+    if not staff:
+        print(f'Staff with ID [ {staff_id} ] not found!')
+        return
+    return staff
+
+
+def authenticate_staff(staff_id, password):
+    staff: Staff | None = get_staff_by_id(staff_id)
+    if staff:
+        return staff.check_password(password)
+    return False
