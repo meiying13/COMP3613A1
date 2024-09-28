51d51059cb39cae8aa52ea153ce0321102ce5791
Felicia Chan <feliciac@davyntt.com>
2024-09-27 20:10:22 -0400

    updated staff model

--- App/models/staff.py
+++ App/models/staff.py
@@ -3,12 +3,13 @@ from .user import User
 
 class Staff(User):
     __tablename__ = "staff"
-    staff_id = db.Column(db.String(8), unique=True, nullable=False)
-    firstname = db.Column(db.String(20), nullable=False)
-    lastname = db.Column(db.String(20), nullable=False)
+    staff_id = db.Column(db.String(8), unique=True)
+    firstname = db.Column(db.String(20))
+    lastname = db.Column(db.String(20))
     written_reviews = db.relationship('Review', backref='author', lazy=True)
+
     __mapper_args__ = {
-        'polymorphic_identity': 'staff'
+        'polymorphic_identity': 'staff', 
     }
 
     def __init__(self, firstname, lastname, staff_id, password):
@@ -16,19 +17,19 @@ class Staff(User):
         self.staff_id = staff_id
         self.firstname = firstname
         self.lastname = lastname
-        
-    def __repr__(self):
-        return f'<Staff: {self.user.id} | {self.staff_id} | {self.get_fullname()}>'
-    
+
     def get_fullname(self):
         return f'{self.firstname} {self.lastname}'
 
     def get_json(self):
         student_reviews = [review.get_json() for review in self.written_reviews]
         return {
-            'id': self.user.id,
+            'id': self.id,
             'staff_id': self.staff_id,
             'firstname': self.firstname,
             'lastname': self.lastname,
             'written_reviews': student_reviews
         }
+        
+    def __repr__(self):
+        return f'< Staff: {self.user.id} | {self.staff_id} | {self.get_fullname()} >'
